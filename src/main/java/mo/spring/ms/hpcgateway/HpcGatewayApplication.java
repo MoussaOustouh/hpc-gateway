5c9b809fcf6e9283712a33c55fb9717e38a134c5
Moussa OUSTOUH <moussa.oustouh@gmail.com>
2021-10-16 18:46:32 +0100

    enable spring cloud discovery (Eureka client) and configure the route statically using RouteLocatorBuilder

--- src/main/java/mo/spring/ms/hpcgateway/HpcGatewayApplication.java
+++ src/main/java/mo/spring/ms/hpcgateway/HpcGatewayApplication.java
@@ -13,11 +13,11 @@ public class HpcGatewayApplication {
         SpringApplication.run(HpcGatewayApplication.class, args);
     }
 
-//    @Bean
-//    RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder){
-//        return routeLocatorBuilder.routes()
-//                .route(r -> r.path("/customers/**").uri("http://localhost:8081/"))
-//                .route(r -> r.path("/products/**").uri("http://localhost:8082/"))
-//                .build();
-//    }
+    @Bean
+    RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder){
+        return routeLocatorBuilder.routes()
+                .route(r -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+                .route(r -> r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
+                .build();
+    }
 }

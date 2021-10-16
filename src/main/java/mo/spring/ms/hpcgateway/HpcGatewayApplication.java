f00a92b2b3e4e8df85025f10cc8300c723f1ad68
Moussa OUSTOUH <moussa.oustouh@gmail.com>
2021-10-16 02:42:40 +0100

    and config gateway routes using the RouteLocatorBuilder class

--- src/main/java/mo/spring/ms/hpcgateway/HpcGatewayApplication.java
+++ src/main/java/mo/spring/ms/hpcgateway/HpcGatewayApplication.java
@@ -2,6 +2,9 @@ package mo.spring.ms.hpcgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HpcGatewayApplication {
@@ -10,4 +13,11 @@ public class HpcGatewayApplication {
         SpringApplication.run(HpcGatewayApplication.class, args);
     }
 
+    @Bean
+    RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder){
+        return routeLocatorBuilder.routes()
+                .route(r -> r.path("/customers/**").uri("http://localhost:8081/"))
+                .route(r -> r.path("/products/**").uri("http://localhost:8082/"))
+                .build();
+    }
 }

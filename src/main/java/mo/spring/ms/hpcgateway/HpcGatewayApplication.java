9bff1d72bb99a356ab0bdff99070a30760247e53
Moussa OUSTOUH <moussa.oustouh@gmail.com>
2021-10-16 01:53:12 +0100

    first commit

---
+++ src/main/java/mo/spring/ms/hpcgateway/HpcGatewayApplication.java
@@ -0,0 +1,13 @@
+package mo.spring.ms.hpcgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpcGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HpcGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mo/spring/ms/hpcgateway/HpcGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package mo.spring.ms.hpcgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpcGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
